d6e84955c2c96c78c45c3d667c8a8ee5996b3337
Jeon Ye-Chan <skyvvv624@naver.com>
2026-09-14 21:07:50 +0900

    Update 2026.09.14

---
+++ Lipository/Lipository.App/Globals/NavigationMapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lipository.App.Globals
+{
+    public class NavigationMapper
+    {
+        /// <summary>
+        /// A dictionary that maps page names to their corresponding types and display names. <br/>
+        /// Key: Page name (string) <br/>
+        /// Value: Tuple containing the page type (Type) and display name (string) <br/>
+        /// </summary>
+        public static Dictionary<string, (Type, string)> PageMap { get; } = new Dictionary<string, (Type, string)>()
+        {
+             { $"{Constants.Pages.RootPath}.MainPage", (typeof(Pages.MainPage), "Main") },
+             { $"{Constants.Pages.RootPath}.DatabasePage", (typeof(Pages.DatabasePage), "Database") },
+             { $"{Constants.Pages.RootPath}.SettingPage", (typeof(Pages.SettingPage), "Settings") }
+        };
+    }
+}
---
+++ Lipository/Lipository.App/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+
+using Lipository.App.Globals;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Lipository.App
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NvMain_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected)
+            {
+                sender.Header = "Settings";
+                mainFrame.Navigate(typeof(Pages.SettingPage));
+            }
+            else if (args.SelectedItem is NavigationViewItem selectedItem &&
+                selectedItem.Tag is string tag)
+            {
+                string pageName = $"{Constants.Pages.RootPath}.{tag}";
+                if (NavigationMapper.PageMap.TryGetValue(pageName, out (Type, string) mapData))
+                {
+                    sender.Header = mapData.Item2;
+                    mainFrame.Navigate(mapData.Item1);
+                }
+            }
+        }
+    }
+}
